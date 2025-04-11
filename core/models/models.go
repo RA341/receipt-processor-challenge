2216d007d7271e7a833fb4678616da21a1a26605
RA341 <razerishi1505@gmail.com>
2025-04-10 21:13:36 -0400

    initial models

---
+++ core/models/models.go
@@ -0,0 +1,22 @@
+package models
+
+type PointsResponse struct {
+	Points int64 `json:"points"`
+}
+
+type IdResponse struct {
+	Id string `json:"id"`
+}
+
+type Receipt struct {
+	Retailer     string `json:"retailer"`
+	PurchaseDate string `json:"purchaseDate"`
+	PurchaseTime string `json:"purchaseTime"`
+	Items        []Item `json:"items"`
+	Total        string `json:"total"`
+}
+
+type Item struct {
+	ShortDescription string `json:"shortDescription"`
+	Price            string `json:"price"`
+}
