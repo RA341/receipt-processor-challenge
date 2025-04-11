98d88395a2d39a76aa3a8fb4c0d1c8c3134ffbd5
RA341 <razerishi1505@gmail.com>
2025-04-10 21:32:44 -0400

    removed testify

--- core/service/points_calculation_test.go
+++ core/service/points_calculation_test.go
@@ -2,68 +2,94 @@ package service
 
 import (
 	"github.com/RA341/receipt-processor-challenge/models"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestCalculations1(t *testing.T) {
-	rec := models.Receipt{
-		Retailer:     "Target",
-		PurchaseDate: "2022-01-01",
-		PurchaseTime: "13:01",
-		Items: []models.Item{
-			{
-				ShortDescription: "Mountain Dew 12PK",
-				Price:            "6.49",
-			}, {
-				ShortDescription: "Emils Cheese Pizza",
-				Price:            "12.25",
-			}, {
-				ShortDescription: "Knorr Creamy Chicken",
-				Price:            "1.26",
-			}, {
-				ShortDescription: "Doritos Nacho Cheese",
-				Price:            "3.35",
-			}, {
-				ShortDescription: "   Klarbrunn 12-PK 12 FL OZ  ",
-				Price:            "12.00",
-			},
-		},
-		Total: "35.35",
-	}
+type TestCase struct {
+	receipt        models.Receipt
+	expectedPoints int64
+}
 
-	result := calculatePoints(&rec, defaultPointRules...)
-	expected := int64(28)
-	assert.Equal(t, expected, result)
+var (
+	testMap = map[string]TestCase{
+		"test 1": {
+			receipt: models.Receipt{
+				Retailer:     "Target",
+				PurchaseDate: "2022-01-01",
+				PurchaseTime: "13:01",
+				Items: []models.Item{
+					{
+						ShortDescription: "Mountain Dew 12PK",
+						Price:            "6.49",
+					}, {
+						ShortDescription: "Emils Cheese Pizza",
+						Price:            "12.25",
+					}, {
+						ShortDescription: "Knorr Creamy Chicken",
+						Price:            "1.26",
+					}, {
+						ShortDescription: "Doritos Nacho Cheese",
+						Price:            "3.35",
+					}, {
+						ShortDescription: "   Klarbrunn 12-PK 12 FL OZ  ",
+						Price:            "12.00",
+					},
+				},
+				Total: "35.35",
+			},
+			expectedPoints: 28,
+		},
+		"test 2": {
+			receipt: models.Receipt{
+				Retailer:     "M&M Corner Market",
+				PurchaseDate: "2022-03-20",
+				PurchaseTime: "14:33",
+				Items: []models.Item{
+					{
+						ShortDescription: "Gatorade",
+						Price:            "2.25",
+					},
+					{
+						ShortDescription: "Gatorade",
+						Price:            "2.25",
+					},
+					{
+						ShortDescription: "Gatorade",
+						Price:            "2.25",
+					},
+					{
+						ShortDescription: "Gatorade",
+						Price:            "2.25",
+					},
+				},
+				Total: "9.00",
+			},
+			expectedPoints: 109,
+		},
+	}
+)
+
+func TestCalculatePointsAll(t *testing.T) {
+	for name, test := range testMap {
+		t.Run(name, func(t *testing.T) {
+			runTest(t, test)
+		})
+	}
+}
+
+func TestCalculations1(t *testing.T) {
+	testCase := testMap["test 1"]
+	runTest(t, testCase)
 }
 
 func TestCalculations2(t *testing.T) {
-	rec := models.Receipt{
-		Retailer:     "M&M Corner Market",
-		PurchaseDate: "2022-03-20",
-		PurchaseTime: "14:33",
-		Items: []models.Item{
-			{
-				ShortDescription: "Gatorade",
-				Price:            "2.25",
-			},
-			{
-				ShortDescription: "Gatorade",
-				Price:            "2.25",
-			},
-			{
-				ShortDescription: "Gatorade",
-				Price:            "2.25",
-			},
-			{
-				ShortDescription: "Gatorade",
-				Price:            "2.25",
-			},
-		},
-		Total: "9.00",
-	}
+	testCase := testMap["test 2"]
+	runTest(t, testCase)
+}
 
-	result := calculatePoints(&rec, defaultPointRules...)
-	expected := int64(109)
-	assert.Equal(t, expected, result)
+func runTest(t *testing.T, testCase TestCase) {
+	result := calculatePoints(&testCase.receipt, defaultPointRules...)
+	if result != testCase.expectedPoints {
+		t.Fatalf("Expected %v but got %v", testCase.expectedPoints, result)
+	}
 }
