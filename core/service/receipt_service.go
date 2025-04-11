5d9415249543256542aa92f42f35698951bc61e8
RA341 <razerishi1505@gmail.com>
2025-04-10 21:13:22 -0400

    initial receipt service

---
+++ core/service/receipt_service.go
@@ -0,0 +1,41 @@
+package service
+
+import "github.com/RA341/receipt-processor-challenge/models"
+
+var (
+	defaultPointRules = []calculationOpts{
+		pointsForRetailerName(),
+		pointsForRoundTotal(),
+		pointsForTotalMultipleOfQuarter(),
+		pointsPerTwoItems(),
+		pointsForItemDescriptionLength(),
+		pointsForOddPurchaseDay(),
+		pointsForPurchaseTimeBetween2And4PM(),
+	}
+)
+
+type ReceiptService struct {
+	db Database
+}
+
+func NewReceiptService(db Database) *ReceiptService {
+	return &ReceiptService{db: db}
+}
+
+func (s *ReceiptService) GetPointsById(transactionId string) (totalPoints int64, err error) {
+	return s.db.GetPointById(transactionId)
+}
+
+func (s *ReceiptService) NewReceipt(receipt models.Receipt) (transactionId string, err error) {
+	finalPoints := calculatePoints(
+		&receipt,
+		defaultPointRules...,
+	)
+
+	pointId, err := s.db.CreatePoint(finalPoints)
+	if err != nil {
+		return "", err
+	}
+
+	return pointId, nil
+}
